4085eb18efecd9bf44b7c06d1ecf6bdde1da5a31
Ayush Rakesh <ayushrakesh35711artlover@gmail.com>
2024-06-05 15:04:50 +0530

    updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am passionate about learning new technologies, solving complex problems and creating scalable solutions.",
   github: "https://github.com/ayushrakesh",
   resume:
-    "https://drive.google.com/file/d/1AXQqX1v8sLBmRPHlSHIQa13mAj0VBV70/view?usp=sharing",
+    "https://drive.google.com/file/d/1gq8oW7gXwicetuxeIOMp9s-6iRN-kr3E/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/ayush-rakesh-005b55228/",
 };
 
